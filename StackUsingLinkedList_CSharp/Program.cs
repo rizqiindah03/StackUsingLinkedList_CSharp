4296848a973e93330914324bf75c5d7ee48ec25e
rizqiindah03 <rizqiindah32@gmail.com>
2022-12-02 16:27:54 +0700

    membuat method pop

--- StackUsingLinkedList_CSharp/Program.cs
+++ StackUsingLinkedList_CSharp/Program.cs
@@ -40,6 +40,11 @@ namespace StackUsingLinkedList_CSharp
             top = fresh;
             Console.WriteLine("\n" + element + "pushed.");
         }
+        public void pop()
+        {
+            Console.WriteLine("\nThe popped element is: " + top.info);
+            top = top.next; //Make top point to the next node is sequence
+        }
     }
 
 }
