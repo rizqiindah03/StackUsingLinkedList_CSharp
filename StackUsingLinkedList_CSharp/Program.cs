3ef1d978cc64a7db82b95f8e4153314557b18fa0
rizqiindah03 <rizqiindah32@gmail.com>
2022-12-02 16:20:39 +0700

    Add project files.

---
+++ StackUsingLinkedList_CSharp/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackUsingLinkedList_CSharp
+{
+   class Node
+    {
+        public int info;
+        public Node next;
+        public Node(int i, Node n)
+        {
+            info = i;
+            next = n;
+        }       
+    }
+
+
+}
