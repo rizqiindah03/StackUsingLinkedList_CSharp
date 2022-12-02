4bda8a1a36cb7a47dc31d4b74abe8182a58d5232
rizqiindah03 <rizqiindah32@gmail.com>
2022-12-02 16:44:04 +0700

    membuat case 1

--- StackUsingLinkedList_CSharp/Program.cs
+++ StackUsingLinkedList_CSharp/Program.cs
@@ -74,7 +74,14 @@ namespace StackUsingLinkedList_CSharp
                 Console.Write("\nEnter your choice: ");
                 string sInput = Console.ReadLine();
                 char ch = Convert.ToChar(sInput == "" ? "0" : sInput);
-
+                switch (ch)
+                {
+                    case '1':
+                        Console.Write("\nEnter a number: ");
+                        int num = Convert.ToInt32(Console.ReadLine());
+                        s.push(num);
+                        break;
+                }
             }
         }
     }
